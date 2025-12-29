8ff70c43c157c838e71dd4a7ee9d7b385340a384
diahkhoeriah <diah.khoeriah@mncgroup.com>
2025-12-29 16:43:05 +0700

    fix case-sensitive import and alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
